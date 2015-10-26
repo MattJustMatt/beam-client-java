11a4586802bed1b415f56d0048311464ef1391a0
Taylor Blau <me@ttaylorr.com>
2015-10-26 12:41:55 -0400

    temp

--- src/main/java/pro/beam/api/resource/chat/AbstractChatDatagram.java
+++ src/main/java/pro/beam/api/resource/chat/AbstractChatDatagram.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 public abstract class AbstractChatDatagram {
     public Type type;
 
-    public static enum Type {
+    public static enum Type extends AbstractChatDatagram {
         @SerializedName("method") METHOD,
         @SerializedName("event") EVENT,
         @SerializedName("reply") REPLY,
